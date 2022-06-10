b8a8c13d52c62b7a29b6727a2b6c7def4f45cfe8
Invincible <571574085@qq.com>
2022-06-10 11:55:27 +0800

    Update basic.vue

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -11,7 +11,7 @@ const tableData = [
   {
     date: '2016-05-03',
     name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    address: 'No. 189, Grove St, Los Angele s',
   },
   {
     date: '2016-05-02',
